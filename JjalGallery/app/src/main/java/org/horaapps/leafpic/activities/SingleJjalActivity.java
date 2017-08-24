656a0277218a1d90a9852f977c86c39ddd95e6ee
YY <nasos9111@gmail.com>
2017-08-24 17:43:18 +0900

    add share func

--- JjalGallery/app/src/main/java/org/horaapps/leafpic/activities/SingleJjalActivity.java
+++ JjalGallery/app/src/main/java/org/horaapps/leafpic/activities/SingleJjalActivity.java
@@ -50,7 +50,6 @@ import org.horaapps.leafpic.views.HackyViewPager;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -388,18 +387,7 @@ public class SingleJjalActivity extends SharedMediaActivity {
 
 
             case R.id.action_share:
-//                Intent share = new Intent(Intent.ACTION_SEND);
-//                Uri uri = Uri.parse(jjalImageItems.get(position).getUrl());
-//                share.setType("image/*");
-//                share.putExtra(Intent.EXTRA_STREAM, uri);
-//                startActivity(Intent.createChooser(share, getString(R.string.send_to)));
-
-
-//                ImageView ivImage = (ImageView) findViewById(R.id.ivResult);
-//                PhotoDraweeView img = new PhotoDraweeView(getApplicationContext());
-//
-
-//                Bitmap imgBitmap = GetImageFromURL()
+                new ImageDownloadAndShare().execute(jjalImageItems.get(position).getUrl());
 
                 break;
             case R.id.action_bookmark:
@@ -438,50 +426,28 @@ public class SingleJjalActivity extends SharedMediaActivity {
         return super.onOptionsItemSelected(item);
     }
 
-//    public Bitmap getBitmapFromURL(String src) {
-//        HttpURLConnection connection = null;
-//        try {
-//            URL url = new URL(src);
-//            connection = (HttpURLConnection) url.openConnection();
-//            connection.setDoInput(true);
-//            connection.connect();
-//            InputStream input = connection.getInputStream();
-//            Bitmap myBitmap = BitmapFactory.decodeStream(input);
-//            return myBitmap;
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            return null;
-//        } finally {
-//            if (connection != null) connection.disconnect();
-//        }
-//    }
-//
-//
-//    public void shareBitmap(Bitmap bitmap) {
-//        String path = MediaStore.Images.Media.insertImage(getContentResolver(),
-//                bitmap, "Image Description", null);
-//        Uri bmpUri = Uri.parse(path);
-//        Intent shareIntent = new Intent(Intent.ACTION_SEND);
-//        shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
-//        shareIntent.setType("image/*");
-//
-//        startActivity(Intent.createChooser(shareIntent, "Share Image"));
-//    }
+    private Bitmap getBitmap(String url) {
+        URL imgUrl = null;
+        HttpURLConnection connection = null;
+        InputStream is = null;
 
-    public Bitmap getBitmapFromURL(String imageUrl) {
+        Bitmap retBitmap = null;
+
         try {
-            URL url = new URL(imageUrl);
-            HttpURLConnection connection = (HttpURLConnection)
-
-                    url.openConnection();
-            connection.setDoInput(true);
-            connection.connect();
-            InputStream inputStream = connection.getInputStream();
-            Bitmap imageBitmap = BitmapFactory.decodeStream(inputStream);
-            return imageBitmap;
-        } catch (IOException e) {
+            imgUrl = new URL(url);
+            connection = (HttpURLConnection) imgUrl.openConnection();
+            connection.setDoInput(true); //url로 input받는 flag 허용
+            connection.connect(); //연결
+            is = connection.getInputStream(); // get inputstream
+            retBitmap = BitmapFactory.decodeStream(is);
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+            return retBitmap;
         }
     }
 
@@ -565,21 +531,8 @@ public class SingleJjalActivity extends SharedMediaActivity {
         protected void onPostExecute(Void result) {
 
             super.onPostExecute(result);
-//
-            //저장한 이미지 열기
-
-//            Intent i = new Intent(Intent.ACTION_VIEW);
-//
-//            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
             String targetDir = Environment.getExternalStorageDirectory().toString() + SAVE_FOLDER;
             File file = new File(targetDir + "/" + fileName + ".jpg");
-
-            //type 지정 (이미지)
-//
-//            i.setDataAndType(Uri.fromFile(file), "image/*");
-//
-//            getApplicationContext().startActivity(i);
 
 
             //이미지 스캔해서 갤러리 업데이트
@@ -608,4 +561,169 @@ public class SingleJjalActivity extends SharedMediaActivity {
     }
 
 
+
+    private class ImageDownloadAndShare extends AsyncTask<String, Void, Void> {
+
+
+
+        /**
+
+         * 파일명
+
+         */
+
+        private String fileName;
+
+
+
+        /**
+
+         * 저장할 폴더
+
+         */
+
+        private final String SAVE_FOLDER = "/save_folder";
+
+
+
+        @Override
+
+        protected Void doInBackground(String... params) {
+
+
+
+            //다운로드 경로를 지정
+
+            String savePath = Environment.getExternalStorageDirectory().toString() + SAVE_FOLDER;
+
+
+
+            File dir = new File(savePath);
+
+            //상위 디렉토리가 존재하지 않을 경우 생성
+
+            if (!dir.exists()) {
+
+                dir.mkdirs();
+
+            }
+
+
+
+            //파일 이름 :날짜_시간
+
+            Date day = new Date();
+
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.KOREA);
+
+            fileName = String.valueOf(sdf.format(day));
+
+
+
+            //웹 서버 쪽 파일이 있는 경로
+
+            String fileUrl = params[0];
+
+
+
+            //다운로드 폴더에 동일한 파일명이 존재하는지 확인
+
+            if (new File(savePath + "/" + fileName).exists() == false) {
+
+            } else {
+
+            }
+
+
+
+            String localPath = savePath + "/" + fileName + ".jpg";
+
+
+
+            try {
+
+                URL imgUrl = new URL(fileUrl);
+
+                //서버와 접속하는 클라이언트 객체 생성
+
+                HttpURLConnection conn = (HttpURLConnection)imgUrl.openConnection();
+
+                int len = conn.getContentLength();
+
+                byte[] tmpByte = new byte[len];
+
+                //입력 스트림을 구한다
+
+                InputStream is = conn.getInputStream();
+
+                File file = new File(localPath);
+
+                //파일 저장 스트림 생성
+
+                FileOutputStream fos = new FileOutputStream(file);
+
+                int read;
+
+                //입력 스트림을 파일로 저장
+
+                for (;;) {
+
+                    read = is.read(tmpByte);
+
+                    if (read <= 0) {
+
+                        break;
+
+                    }
+
+                    fos.write(tmpByte, 0, read); //file 생성
+
+                }
+
+                is.close();
+
+                fos.close();
+
+                conn.disconnect();
+
+            } catch (Exception e) {
+
+                e.printStackTrace();
+
+            }
+
+
+
+            return null;
+
+        }
+
+
+
+        @Override
+
+        protected void onPostExecute(Void result) {
+
+            super.onPostExecute(result);
+
+
+
+            //저장한 이미지 열기
+
+            Intent share = new Intent(Intent.ACTION_SEND);
+
+            String targetDir = Environment.getExternalStorageDirectory().toString() + SAVE_FOLDER;
+
+            File file = new File(targetDir + "/" + fileName + ".jpg");
+
+            //type 지정 (이미지)
+
+            share.setDataAndType(Uri.fromFile(file), "image/*");
+            share.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
+            startActivity(Intent.createChooser(share, getString(R.string.send_to)));
+
+
+        }
+
+    }
 }
